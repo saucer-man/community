6926c96eb6f6fb522eba441c6416bc892f6279cd
unknown <yanqing.geng@yitu-inc.com>
2019-11-08 16:51:22 +0800

    add session to sql

--- src/main/java/com/saucerman/community/controller/AuthorizeController.java
+++ src/main/java/com/saucerman/community/controller/AuthorizeController.java
@@ -11,8 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.jws.soap.SOAPBinding;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -35,7 +36,8 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state,
-                           HttpServletRequest request) {
+                           HttpServletRequest request,
+                           HttpServletResponse response) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -47,14 +49,15 @@ public class AuthorizeController {
         if (githubUser != null) {
             //登陆成功，写session和cookie
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
 
             userMapper.insert(user);
-            request.getSession().setAttribute("githubUser", githubUser);
+            response.addCookie(new Cookie("token", token));
             return "redirect:/";
 
         } else {
--- src/main/java/com/saucerman/community/controller/IndexController.java
+++ src/main/java/com/saucerman/community/controller/IndexController.java
@@ -1,14 +1,33 @@
 package com.saucerman.community.controller;
 
+import com.saucerman.community.mapper.UserMapper;
+import com.saucerman.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String Index(){
+    public String Index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for(Cookie cookie:cookies){
+            if(cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if(user!=null){
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
+
         return "index";
     }
 }
