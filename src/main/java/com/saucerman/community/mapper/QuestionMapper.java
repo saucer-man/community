08288f7f7ce5457524a161d0ffbeb9f60bb506c0
saucerman <gyq0929@gmail.com>
2019-11-13 15:44:35 +0800

    add publish action

---
+++ src/main/java/com/saucerman/community/mapper/QuestionMapper.java
@@ -0,0 +1,11 @@
+package com.saucerman.community.mapper;
+
+import com.saucerman.community.model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface QuestionMapper {
+    @Insert("insert into question (title,description,gmt_create,gmt_modified,creator,tag) values (#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})")
+    void create(Question question);
+}
---
+++ src/main/java/com/saucerman/community/model/Question.java
@@ -0,0 +1,97 @@
+package com.saucerman.community.model;
+
+public class Question {
+    private Integer id; // id
+    private String title; //标题
+    private String description;  //描述
+    private String tag; //标签
+    private Long gmtCreate;  //创建时间
+    private Long gmtModified;  // 修改时间
+    private Integer creator;  // 创建的人
+    private Integer viewCount; // 浏览数
+    private Integer commentCount; // 评论数
+    private Integer likeCount; // 点赞数
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public Long getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Long gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public Long getGmtModified() {
+        return gmtModified;
+    }
+
+    public void setGmtModified(Long gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+
+    public Integer getCreator() {
+        return creator;
+    }
+
+    public void setCreator(Integer creator) {
+        this.creator = creator;
+    }
+
+    public Integer getViewCount() {
+        return viewCount;
+    }
+
+    public void setViewCount(Integer viewCount) {
+        this.viewCount = viewCount;
+    }
+
+    public Integer getCommentCount() {
+        return commentCount;
+    }
+
+    public void setCommentCount(Integer commentCount) {
+        this.commentCount = commentCount;
+    }
+
+    public Integer getLikeCount() {
+        return likeCount;
+    }
+
+    public void setLikeCount(Integer likeCount) {
+        this.likeCount = likeCount;
+    }
+
+
+}
