f58931bd51d2a7ec9c0dcffa5863a3a521d00708
saucer-man <gyq0929@gmail.com>
2019-12-31 20:53:54 +0800

    添加拦截器intercept

--- src/main/java/com/saucerman/community/controller/IndexController.java
+++ src/main/java/com/saucerman/community/controller/IndexController.java
@@ -1,11 +1,6 @@
 package com.saucerman.community.controller;
 
 import com.saucerman.community.dto.PaginationDTO;
-import com.saucerman.community.dto.QuestionDTO;
-import com.saucerman.community.mapper.QuestionMapper;
-import com.saucerman.community.mapper.UserMapper;
-import com.saucerman.community.model.Question;
-import com.saucerman.community.model.User;
 import com.saucerman.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,41 +8,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
-
 @Controller
 public class IndexController {
-    @Autowired
-    private UserMapper userMapper;
 
     @Autowired
     private QuestionService questionService;
 
     @GetMapping("/")
-    public String Index(HttpServletRequest request,
-                        Model model,
+    public String Index(Model model,
                         @RequestParam(name = "page", defaultValue = "1") Integer page,
                         @RequestParam(name = "size", defaultValue = "5") Integer size){
 
-        //检验登录状态
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null && cookies.length != 0) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    User user = userMapper.findByToken(token);
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-                }
-            }
-        }
-
         //查出数据放在页面里面
-
         PaginationDTO pagination = questionService.list(page, size);
         model.addAttribute("pagination", pagination);
         return "index";
